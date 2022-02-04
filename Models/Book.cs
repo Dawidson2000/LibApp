980a2cea82044cd2fe59f79035ab15108a07633a
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2022-02-04 12:01:05 +0100

    Added validation messages form BookFor in viewm

--- Models/Book.cs
+++ Models/Book.cs
@@ -26,7 +26,7 @@ namespace LibApp.Models
 		[Required(ErrorMessage = "Release date is required.")]
 		public DateTime ReleaseDate { get; set; }
 
-		[Required(ErrorMessage = "Number in stock is required.")]
+		[Required]
 		[Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
 		public int NumberInStock { get; set; }
 		
