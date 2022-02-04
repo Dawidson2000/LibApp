77962d9cefdd6309e5a33c0be480f9f62142af9e
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2022-02-04 11:41:47 +0100

    Add error messages to BookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,17 +9,27 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		
+		[Required(ErrorMessage = "Name is required.")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		
+		[Required(ErrorMessage = "Author Name is required.")]
 		public string AuthorName { get; set; }
-		[Required]
+		
+		[Required(ErrorMessage = "Genre is required.")]
 		public Genre Genre { get; set; }
+		
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		
+		[Required(ErrorMessage = "Release date is required.")]
 		public DateTime ReleaseDate { get; set; }
+
+		[Required(ErrorMessage = "Number in stock is required.")]
+		[Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
 		public int NumberInStock { get; set; }
+		
 		public int NumberAvailable { get; set; }
 	}
       
