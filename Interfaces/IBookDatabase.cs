c2286577e45ff502e45d1a228463d3ae8cd3e980
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2022-02-04 13:54:03 +0100

    Added IBookDatabase interface

---
+++ Interfaces/IBookDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using LibApp.Models;
+namespace LibApp.Interfaces
+{
+    public interface IBookDatabase
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookById(int id);
+        void AddBook(Book book);
+        void DeleteBook(int id);
+        void UpdateBook(Book book);
+        void Save();
+    }
+}
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,29 +25,57 @@ namespace LibApp.Models
                         Id = 1,
                         SignUpFee = 0,
                         DurationInMonths = 0,
-                        DiscountRate = 0
+                        DiscountRate = 0,
+                        Name = "New",
                     },
                     new MembershipType
                     {
                         Id = 2,
                         SignUpFee = 30,
                         DurationInMonths = 1,
-                        DiscountRate = 10
+                        DiscountRate = 10,
+                        Name = "Average",
                     },
                     new MembershipType
                     {
                         Id = 3,
                         SignUpFee = 90,
                         DurationInMonths = 3,
-                        DiscountRate = 15
+                        DiscountRate = 15,
+                        Name = "Special",
                     },
                     new MembershipType
                     {
                         Id = 4,
                         SignUpFee = 300,
                         DurationInMonths = 12,
-                        DiscountRate = 20
+                        DiscountRate = 20,
+                        Name = "Premium",
                     });
+                context.SaveChanges();
+
+                if (context.Customers.Any())
+                {
+                    Console.WriteLine("Database already seeded2");
+                    return;
+                }
+
+                context.Customers.AddRange(
+                    new Customer
+                    {   
+                        Name = "Marek",
+                        HasNewsletterSubscribed = false,
+                        MembershipTypeId = 1,
+                        Birthdate = new DateTime(2000, 02, 02)
+                    },
+                    new Customer
+                    {
+                        Name = "Dawid",
+                        HasNewsletterSubscribed = true,
+                        MembershipTypeId = 2,
+                        Birthdate = new DateTime(2002, 04, 12)
+                    });
+                
                 context.SaveChanges();
             }
         }
